c89937a1ac1efd4a2926a77c3be4e980a8f286d5
Atal Kumar <atalkumar.me@gmail.com>
2025-10-23 16:16:09 +0530

    fix(TestimonialCard): updated and fix glitch in UI

--- src/components/common/TestimonialCard.js
+++ src/components/common/TestimonialCard.js
@@ -12,7 +12,9 @@ const TestimonialCard = ({ name, role, company, avatar, rating, text, position, 
             <p className="testimonial-card__role">{role}, {company}</p>
             <div className="testimonial-card__rating">
               {[...Array(rating)].map((_, i) => (
-                <span key={i} className="testimonial-card__star">⭐</span>
+                <span key={i} className="testimonial-card__star"><svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2L9.19 8.63L2 9.24l5.46 4.73L5.82 21z" />
+              </svg></span>
               ))}
             </div>
           </div>
